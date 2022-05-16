6c43f533c7ae5238614951bd5bafb16269d745ea
ameya <ameya@ameyas-MacBook-Air.local>
2022-05-16 16:33:45 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const { v4: uuidv4 } = require('uuid');
+const io = require('socket.io')(server);
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
+app.use('/peerjs', peerServer);
+
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).emit('user-connected', userId);
+        socket.on('message', message => {
+            io.to(roomId).emit('createMesssage', message)
+        })
+    })
+})
+
+
+server.listen(3030);
